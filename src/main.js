8a0b384f85a03239aefb1485fc93fab493826481
Dmitry <dima747@gmail.com>
2018-03-04 14:52:21 +0200

    edit date time of meetup

--- src/main.js
+++ src/main.js
@@ -8,12 +8,16 @@ import DateFilter from './filters/date'
 import * as Firebase from 'firebase'
 import AlertCmp from './components/Shared/Alert'
 import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog'
 
 import colors from 'vuetify/es5/util/colors'
 
 Vue.filter('date', DateFilter)
 Vue.component('app-alert', AlertCmp)
 Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
 
 Vue.use(Vuetify, {
   theme: {
